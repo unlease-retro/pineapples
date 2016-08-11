c3646706625ce7b2972f9f5b6b024caedf189945
Luke Hedger <luke.hedger@gmail.com>
2016-08-11 16:22:11 +0100

    added Aphrodite for inline-styles

--- app/shared/components/header.js
+++ app/shared/components/header.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite/no-important'
 
 const Header = () => (
-  <header>
+  <header className={ css(styles.red) }>
     Unlease :: Pineapples
   </header>
 )
+
+const styles = StyleSheet.create({
+  red: {
+    color: 'lime'
+  }
+})
 
 export default Header
