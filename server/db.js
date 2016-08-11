24def6c58d454a3360af5d86dd01234ef8b46774
Luke Hedger <luke.hedger@gmail.com>
2016-08-11 15:23:08 +0100

    add mongoose connection

--- server/db.js
+++ server/db.js
@@ -1,1 +1,18 @@
-// TODO
+const mongoose = require('mongoose')
+
+const MONGO_HOST = process.env.MONGO_URL || 'mongodb://127.0.0.1/pineapples'
+
+mongoose.connect(MONGO_HOST, {
+  server: { poolSize: 5 },
+  db: { native_parser: true }
+})
+
+// use native promises
+mongoose.Promise = global.Promise
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('db connected')
+})
