88bb1a0a73e1bc51505d4e22a3920d7e9b52fc9b
Luke Hedger <luke.hedger@gmail.com>
2016-08-12 12:35:09 +0100

    added Cluster domain with model

---
+++ server/cluster/constants.js
@@ -0,0 +1,5 @@
+const name = 'cluster'
+
+module.exports = {
+  name,
+}
---
+++ server/cluster/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const Cluster = new Schema({
+  name: { type: String, required: true, unique: true, index: true }
+  depot: { type: String, sparse: true }
+  rider: { type: String, sparse: true }
+  writer: String,
+  centroid: [Number]
+  currentLocation: [Number]
+  deliverable: { type: Boolean, index: true },
+  startedAt: Date,
+  items: [
+    {
+      pineapple: { type: String, index: true },
+      delivered: Boolean,
+      deliveredAt: Date,
+      deliveredTs: Date
+    }
+  ]
+},
+{
+  timestamps: true
+})
+
+module.exports = Cluster
