50373a9eb5b83458f4316014acc494e0a9a0fc58
Luke Hedger <luke.hedger@gmail.com>
2016-08-12 14:12:27 +0100

    added new domains to routes

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 // domains
+const Cluster = require('./cluster')
+const Depot = require('./depot')
 const Pineapple = require('./pineapple')
+const Settings = require('./settings')
+const User = require('./user')
+const Writer = require('./writer')
 
 // app
 router.get('/', (req, res, next) => {
@@ -14,6 +19,11 @@ router.get('/', (req, res, next) => {
 })
 
 // API
-router.use(`api/${Pineapple.name}`, Pineapple.routes)
+router.use(`/api/${Cluster.name}`, Cluster.routes)
+router.use(`/api/${Depot.name}`, Depot.routes)
+router.use(`/api/${Pineapple.name}`, Pineapple.routes)
+router.use(`/api/${Settings.name}`, Settings.routes)
+router.use(`/api/${User.name}`, User.routes)
+router.use(`/api/${Writer.name}`, Writer.routes)
 
 module.exports = router
