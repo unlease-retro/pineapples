16808bcd0053409e8becde13e573a1f49ca839ac
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-08-23 15:15:16 +0100

    Add styles to orders on delivery page

--- app/rider/components/Pineapples.js
+++ app/rider/components/Pineapples.js
@@ -2,6 +2,7 @@
  * Created by BigaMasta on 8/23/16.
  */
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite/no-important'
 
 export default class Pineapples extends React.Component {
 
@@ -15,13 +16,22 @@ export default class Pineapples extends React.Component {
 
           return (
             <div key={item._id}>
-              <p>Address: </p>
-              <p>{`${item.flatNumber} ${item.streetAddress}`}</p>
-              <p>{item.postcode}</p>
-              <p>{item.city}</p>
-              <p>From: {item.from ? item.from : <i>Not specified</i>}</p>
-              <p>To: {item.to}</p>
-              <p>Message: {item.message}</p>
+              <div className={ css(styles.address) }>
+                <h2 className={ css(styles.orderTitle) }>Address: </h2>
+                <p className={ css(styles.orderItem) }>{`${item.flatNumber} ${item.streetAddress}`}</p>
+                <p className={ css(styles.orderItem) }>{item.postcode}</p>
+              </div>
+
+              <div className={ css(styles.orderInfo) }>
+                <h2 className={ css(styles.orderTitle) }>From: </h2>
+                <p className={ css(styles.orderItem) }>{item.from ? item.from : <i>Not specified</i>}</p>
+
+                <h2 className={ css(styles.orderTitle) }>To: </h2>
+                <p className={ css(styles.orderItem) }>{item.to}</p>
+
+                <h2 className={ css(styles.orderTitle) }>Message: </h2>
+                <p className={ css(styles.orderItem) }>{item.message}</p>
+              </div>
             </div>
           )
 
@@ -32,3 +42,24 @@ export default class Pineapples extends React.Component {
   }
 
 }
+
+const styles = StyleSheet.create({
+  orderInfo: {
+    border: '5px solid black',
+    borderRadius: '10px',
+    margin: '0 10px',
+    backgroundColor: '#FEDC81'
+  },
+  orderTitle: {
+    fontSize: '26px',
+    marginLeft: '10px',
+    marginBottom: '5px'
+  },
+  orderItem: {
+    fontSize: '16px',
+    marginLeft: '30px'
+  },
+  address: {
+    margin: '50px 10px'
+  }
+})
