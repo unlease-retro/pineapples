2fdfa8f3580879202cd9d373a69abc7216545018
Izzy <xinjin214@gmail.com>
2016-08-24 10:27:37 +0100

    remove origin check on cors, and change port to 8001

--- server/shared/constants/index.js
+++ server/shared/constants/index.js
@@ -1,6 +1,6 @@
 exports.POSTMARK_KEY = '4304dbb3-74a7-49e9-b417-6ca38b4bdefc'
 exports.HOST = process.env.HOST || 'localhost'
-exports.PORT = process.env.PORT || '8000'
+exports.PORT = process.env.PORT || '8001'
 exports.ROLES = {
   SUPERUSER: 'SUPERUSER',
   MANAGER: 'MANAGER',
