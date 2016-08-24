a770b2e6baa66a29010b65a2e05207c11018afc7
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-08-24 13:32:57 +0100

    Refactor - delivery - widthdraw logic from delivery component about view all button

--- app/rider/container.js
+++ app/rider/container.js
@@ -28,7 +28,7 @@ export class Rider extends Component {
             changeStatus: this.props.actions.changeStatus,
             unselectCluster: this.props.actions.unselectCluster
           }}
-          clusters={this.props.clusters}
+          viewAllButton={this.props.clusters.length > 1}
         />
       </div>
     )
