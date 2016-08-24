300e25962bf6847170d86e0cbac14cdcce50af69
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-08-24 11:17:23 +0100

    If rider is assigned one cluster, select that cluster

--- app/rider/reducer.js
+++ app/rider/reducer.js
@@ -11,7 +11,15 @@ export const initialState = Immutable.fromJS({
 export default createReducer(initialState, {
 
   [actions.FETCH_CLUSTERS_REQUEST]: (state, action) => state.merge({ ...action.payload }),
-  [actions.FETCH_CLUSTERS_SUCCESS]: (state, action) => state.merge({ ...action.payload }),
+  [actions.FETCH_CLUSTERS_SUCCESS]: (state, action) => {
+
+    // if there is only one cluster, select it
+    if (action.payload.clusters.length === 1)
+      return state.merge({ ...action.payload, selectedCluster: Immutable.fromJS(action.payload.clusters[0])})
+    else
+      return state.merge({ ...action.payload })
+
+  },
   [actions.FETCH_CLUSTERS_FAILURE]: (state, action) => state.merge({ ...action.payload }),
 
   [actions.SELECT_CLUSTER]: (state, action) => state.merge({ ...action.payload }),
