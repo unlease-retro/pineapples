d7143b2ced4bf5e424a2eec5d75edfcc90917aac
Izzy <xinjin214@gmail.com>
2016-08-25 18:11:18 +0100

    build part 1

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -41,7 +41,7 @@ module.exports = {
       }
     }),
     new CleanPlugin([ PATHS.dist ], { root: process.cwd() }),
-    new CopyPlugin([ { from: './static', to: './' } ], { ignore: [ '.*' ] }),
+    new CopyPlugin([ { from: './static', to: './' },{ from: './buildScripts', to: '../buildScripts' } ], { ignore: [ '.*' ] }),
     new HtmlWebpackPlugin({ template: 'app/index.html', filename: 'app.html' })
   ],
 
