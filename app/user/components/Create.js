846e645b597175ca9f1f7d2d865f28bae2e11901
Luke Hedger <luke.hedger@gmail.com>
2016-09-01 17:22:54 +0100

    add create user style

--- app/user/components/Create.js
+++ app/user/components/Create.js
@@ -1,49 +1,32 @@
 import React from 'react'
 import Role from './Role'
 
-import { button as Button } from '../../shared/components'
+import { button as Button, grid as Grid, input as Input } from '../../shared/components'
 
 const Component = ({ role, createWriter, createUser, changeRole, selectedRole }) => {
 
-  const handleSubmit = () => selectedRole === 'WRITER' ? createWriter({ email: Component.email }) : createUser({ firstname: Component.firstname, lastname: Component.lastname, email: Component.email, phone: Component.phone, role: selectedRole })
+  const isWriter = selectedRole === 'WRITER'
 
-  const renderFirstname = selectedRole !== 'WRITER' ?
-    <p>
-      <label htmlFor='firstname'>First name</label>
-      <input type='text' onChange={ e => Component.firstname = e.target.value } name='firstname' placeholder='John' required/>
-    </p> : null
+  const handleSubmit = () => isWriter ? createWriter({ email: Component.email }) : createUser({ firstname: Component.firstname, lastname: Component.lastname, email: Component.email, phone: Component.phone, role: selectedRole })
 
-  const renderLastname = selectedRole !== 'WRITER' ?
-    <p>
-      <label htmlFor='lastname'>Last name</label>
-      <input type='text' onChange={ e => Component.lastname = e.target.value } name='lastname' placeholder='Doe' required/>
-    </p> : null
-
-  const renderPhone = selectedRole !== 'WRITER' ?
-    <p>
-      <label htmlFor='phone'>Phone</label>
-      <input type='tel' onChange={ e => Component.phone = e.target.value } name='phone' placeholder='(555) 555-5555' required/>
-    </p> : null
+  const renderFirstname = !isWriter ? <Input placeholder='Firstname' onChange={ e => Component.firstname = e.target.value } /> : null
+  const renderLastname = !isWriter ? <Input placeholder='Lastname' onChange={ e => Component.lastname = e.target.value } /> : null
+  const renderPhone = !isWriter ? <Input type='tel' placeholder='Phone' onChange={ e => Component.phone = e.target.value } /> : null
 
 
   return (
-    <div>
-      <p>
-        <label htmlFor='role'>Role</label>
-        <Role role={role} onChange={ role => changeRole(role) } />
-      </p>
+    <Grid>
+
+      <Role role={role} onChange={ role => changeRole(role) } />
 
       {renderFirstname}
       {renderLastname}
       {renderPhone}
 
-      <p>
-        <label htmlFor='email'>E-mail</label>
-        <input type='email' onChange={ e => Component.email = e.target.value } name='email' placeholder='john@doe.com' required/>
-      </p>
+      <Input type='email' placeholder='Email' onChange={ e => Component.email = e.target.value } />
 
       <Button label='Submit' onClick={handleSubmit} theme='accent' />
-    </div>
+    </Grid>
   )
 
 }
--- app/user/container.js
+++ app/user/container.js
@@ -23,6 +23,8 @@ export class User extends Component {
         <Components.list data={users} update={actions.updateUser} remove={actions.deleteUser} />
         <Components.list data={writers} update={actions.updateWriter} remove={actions.deleteWriter} />
 
+        <h3>Create new user</h3>
+
         <Components.create role={role} selectedRole={selectedRole} changeRole={actions.changeRole} createUser={actions.createUser} createWriter={actions.createWriter} />
 
       </div>
