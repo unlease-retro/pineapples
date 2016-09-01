44b38bc149ac71bde1a61a5ac313050982f38f54
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-09-01 17:18:14 +0100

    Refactor the Status component to use shared button component

--- app/rider/components/Status.js
+++ app/rider/components/Status.js
@@ -2,7 +2,7 @@
  * Created by BigaMasta on 8/24/16.
  */
 import React from 'react'
-import { StyleSheet, css } from 'aphrodite/no-important'
+import { button as Button } from '../../shared/components'
 
 import { DELIVERED, UNDELIVERED } from '../constants'
 
@@ -10,33 +10,16 @@ const Status = ({ item, itemIndex, actions: { changeStatus } }) => {
 
   const buttonLabel = item.delivered ? DELIVERED : UNDELIVERED
 
-  Status._onClick = (item, itemIndex) => {
+  const _onClick = (item, itemIndex) => {
 
     const query = { delivered: !item.delivered }
     changeStatus(item, query, itemIndex)
 
   }
 
-  return (
-    <a className={ css(styles.statusAndMapItem, styles.button) } onClick={ () => Status._onClick(item, itemIndex) }>
-      {buttonLabel}
-    </a>
-  )
+  return <Button label={buttonLabel} onClick={ () => _onClick(item, itemIndex) } />
+
 
 }
-
-const styles = StyleSheet.create({
-  statusAndMapItem: {
-    margin: '15px'
-  },
-  button: {
-    backgroundColor: '#6FC9BC',
-    borderRadius: '5px',
-    padding: '10px',
-    fontSize: '16px',
-    textAlign: 'center',
-    cursor: 'pointer'
-  },
-})
 
 export default Status
