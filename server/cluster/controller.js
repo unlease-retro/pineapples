3a1ced8f7bb4b0a6124975b6c03b67385d3d9b7b
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-09-01 16:17:01 +0100

    Fix when the emails are sent to riders

--- server/cluster/controller.js
+++ server/cluster/controller.js
@@ -139,32 +139,44 @@ exports.writer = (req, res, next) => {
 
 }
 
+
+const ridersEqual = (clusterBefore, clusterAfter) => {
+
+  if (!clusterBefore.rider && clusterAfter.rider)
+    return false
+
+  if (clusterBefore.rider && !clusterAfter.rider)
+    return false
+
+  if (!clusterBefore.rider && !clusterAfter.rider)
+    return true
+
+  if (clusterBefore.rider && clusterAfter.rider)
+    return clusterBefore.rider._id.toString() === clusterAfter.rider._id.toString()
+
+}
+const deliverableEqual = (clusterBefore, clusterAfter) => clusterAfter.deliverable === clusterBefore.deliverable
+
+// Notify rider when assigned to cluster and cluster is deliverable (email template 1)
+// Notify rider when unassigned from cluster and cluster is deliverable (email template 2)
+// Notify rider when cluster assigned to is made deliverable (email template 1)
+// Notify rider when cluster assigned cluster is made undeliverable (email template 2)
 const sendNotificationEmailsIfNeeded = (clusterBefore, clusterAfter) => {
 
-  if (clusterBefore && (typeof clusterBefore) !== 'object')
-    clusterBefore = {_id: clusterBefore}
-  if (clusterAfter && (typeof clusterAfter) !== 'object')
-    clusterAfter = {_id: clusterAfter}
+  if (!ridersEqual(clusterBefore, clusterAfter) && clusterAfter.deliverable) {
 
-  // Notify rider when assigned to cluster and cluster is deliverable (email template 1)
-  // Notify rider when unassigned from cluster and cluster is deliverable (email template 2)
-  // Notify rider when cluster assigned to is made deliverable (email template 1)
-  // Notify rider when cluster assigned cluster is made undeliverable (email template 2)
-
-  if (clusterBefore.rider._id !== clusterAfter.rider._id && clusterAfter.deliverable) {
-
-    if ((typeof clusterBefore.rider._id === 'undefined') && (typeof clusterAfter.rider._id !== 'undefined')) {
+    if (!clusterBefore.rider && clusterAfter.rider) {
 
       EmailService.sendToRiderAfterAssignment(clusterAfter.rider.email, {})
 
     }
-    else if ((typeof clusterBefore.rider._id !== 'undefined') && (typeof clusterAfter.rider._id !== 'undefined')) {
+    else if (clusterBefore.rider && clusterAfter.rider) {
 
       EmailService.sendToRiderAfterAssignment(clusterAfter.rider.email, clusterAfter)
       EmailService.sendToRiderAfterUnassignment(clusterBefore.rider.email, {})
 
     }
-    else if ((typeof clusterBefore.rider._id !== 'undefined') && (typeof clusterAfter.rider._id === 'undefined')) {
+    else if (clusterBefore.rider && !clusterAfter.rider) {
 
       // This brach is never executed, because on the frontend side is not possible to unassign a cluster
       EmailService.sendToRiderAfterUnassignment(clusterBefore.rider.email, {})
@@ -173,7 +185,7 @@ const sendNotificationEmailsIfNeeded = (clusterBefore, clusterAfter) => {
 
   }
 
-  if (clusterAfter.deliverable !== clusterBefore.deliverable && clusterBefore.rider._id === clusterAfter.rider._id) {
+  if (!deliverableEqual(clusterBefore, clusterAfter) && ridersEqual(clusterBefore, clusterAfter)) {
 
     if (clusterAfter.deliverable)
       EmailService.sendToRiderAfterAssignment(clusterAfter.rider.email, {})
