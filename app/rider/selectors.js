abb399b4d157d4fe3e1758032e6fbd9d3e5c3073
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-09-02 15:58:11 +0100

    Sort pineapples in cluster based on route

--- app/rider/selectors.js
+++ app/rider/selectors.js
@@ -1,5 +1,6 @@
 import { name } from './constants'
 import { reasons } from '../shared/constants/index'
+import Immutable from 'immutable'
 
 export const getAll = state => state.get(name).toJS()
 export const getClusters = state => state.get(name).get('clusters').toJS()
@@ -7,6 +8,26 @@ export const getUndeliveredReasonOptions = () => reasons.map(reason => ({ value:
 export const selectedCluster = state => {
 
   const index = state.get(name).get('selectedClusterIndex')
-  return state.getIn([name, 'clusters', index]) && state.getIn([name, 'clusters', index]).toJS()
+  const selectedCluster = state.getIn([name, 'clusters', index])
+
+  if (selectedCluster) {
+
+    const waypointOrder = selectedCluster.getIn(['route', 'waypoint_order'])
+    const items = selectedCluster.get('items')
+    let newOrder = new Immutable.List()
+
+    waypointOrder.map(orderItemIndex => {
+
+      newOrder = newOrder.push(items.get(orderItemIndex))
+
+    })
+
+    const clusterWithOrderedPineapplesState = state.setIn([name, 'clusters', index, 'items'], newOrder)
+
+    return clusterWithOrderedPineapplesState.getIn([name, 'clusters', index]).toJS()
+
+  }
+
+  return selectedCluster
 
 }
