1c785defef16f782bd16c8332221c2ed38e0813b
Luke Hedger <luke.hedger@gmail.com>
2016-09-06 12:33:16 +0100

    update admin service to handle fetch API errors

--- app/admin/service.js
+++ app/admin/service.js
@@ -6,32 +6,36 @@ import * as API from '../shared/services/api'
 
 export const fetchClusters = (resolve, reject) =>
   API.get('cluster')
-    .then( res => resolve(res.json()) )
+    .then( res => res.json() )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json) )
     .catch( e => reject(e) )
 
 export const fetchDepots = (resolve, reject) =>
   API.get('depot')
-    .then( res => resolve(res.json()) )
+    .then( res => res.json() )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json) )
     .catch( e => reject(e) )
 
 export const fetchRiders = (resolve, reject) =>
   API.get('user/rider')
     .then( res => res.json() )
-    .then( json => resolve({ riders: json.users }) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ riders: json.users }) )
     .catch( e => reject(e) )
 
 export const updateCluster = (resolve, reject, id, data, index) =>
   API.put(`cluster/${id}`, data)
     .then( res => res.json() )
-    .then( ({ cluster }) => resolve({ index, cluster, snackbar: 'Cluster updated, no problemo 😁' }) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ index, cluster: json.cluster, snackbar: 'Cluster updated, no problemo 😁' }) )
     .catch( e => reject(e) )
 
 export const cutOff = (resolve, reject) =>
   API.post('cluster/generate')
-    .then( res => resolve(res.json()) )
+    .then( res => res.json() )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json) )
     .catch( e => reject(e) )
 
 export const fetchStats = (resolve, reject) =>
   API.get('pineapple/list/stats')
-    .then( res => resolve(res.json()) )
+    .then( res => res.json() )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json) )
     .catch( e => reject(e) )
