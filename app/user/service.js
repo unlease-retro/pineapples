ff23912b1b1846e964e939b9334bca8b8f15f569
Luke Hedger <luke.hedger@gmail.com>
2016-09-06 12:32:46 +0100

    update user service to handle fetch API errors

--- app/user/service.js
+++ app/user/service.js
@@ -4,55 +4,54 @@
 
 import * as API from '../shared/services/api'
 
-
 export const fetchUser = (resolve, reject) =>
   API.get('auth/whoami')
     .then( res => res.json() )
-    .then( json => resolve(json.user) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json.user) )
     .catch( e => reject(e) )
 
 export const fetchUsers = (role, resolve, reject) =>
   API.get(`user/${role}`)
     .then( res => res.json() )
-    .then( json => resolve(json) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json) )
     .catch( e => reject(e) )
 
 export const createUser = (data, resolve, reject) =>
   API.post('user', data)
     .then( res => res.json() )
-    .then( json => resolve(json.user) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json.user) )
     .catch( e => reject(e) )
 
 export const updateUser = (id, data, index, resolve, reject) =>
   API.put(`user/${id}`, data)
     .then( res => res.json() )
-    .then( json => resolve({ index, user: json.user }) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ index, user: json.user }) )
     .catch( e => reject(e) )
 
 export const deleteUser = (id, index, resolve, reject) =>
   API.remove(`user/${id}`)
-    .then( res => resolve({ index, res }) )
+    .then( res => res.ok ? resolve({ index, res }) : reject(Error('Error deleting writer')) )
     .catch( e => reject(e) )
 
 export const fetchWriters = (resolve, reject) =>
   API.get('writer')
   .then( res => res.json() )
-  .then( json => resolve(json) )
+  .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json) )
   .catch( e => reject(e) )
 
 export const createWriter = (data, resolve, reject) =>
   API.post('writer', data)
     .then( res => res.json() )
-    .then( json => resolve(json.writer) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve(json) )
     .catch( e => reject(e) )
 
 export const updateWriter = (id, data, index, resolve, reject) =>
   API.put(`writer/${id}`, data)
     .then( res => res.json() )
-    .then( json => resolve({ index, writer: json.writer }) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ index, writer: json.writer }) )
     .catch( e => reject(e) )
 
 export const deleteWriter = (id, index, resolve, reject) =>
   API.remove(`writer/${id}`)
-    .then( res => resolve({ index, res }) )
+    .then( res => res.ok ? resolve({ index, res }) : reject(Error('Error deleting writer')) )
     .catch( e => reject(e) )
