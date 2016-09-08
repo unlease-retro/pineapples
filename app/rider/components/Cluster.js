570faa3f4760ba01897a10906b768ee82d8bb8ee
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-09-08 14:32:25 +0100

    [RIDER] Add warning sign on cluster when at least one of the pineapples inside a cluster is undelivered

--- app/rider/components/Cluster.js
+++ app/rider/components/Cluster.js
@@ -8,10 +8,11 @@ import { card as Card, icon as Icon } from '../../shared/components'
 const Cluster = ({ cluster, clusterIndex, actions: { selectCluster } }) => {
 
   const renderDoneTick = cluster.get('finishedAt') ? <Icon name='check_circle' theme='accent'/> : null
+  const renderWarningTick = cluster.get('finishedAt') && cluster.get('hasUndeliveredReasons') ? <Icon name='warning' theme='pineapple'/> : null
 
   return (
     <Card onClick={() => selectCluster(clusterIndex)}>
-      <div>{ cluster.get('name') } {renderDoneTick}</div>
+      <div>{ cluster.get('name') } {renderDoneTick} {renderWarningTick}</div>
     </Card>
   )
 
