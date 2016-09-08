b3b51258eb81a8caf2ba37eb88438981b569240e
Luke Hedger <luke.hedger@gmail.com>
2016-09-08 15:05:50 +0100

    update todays orders count to exclude undeliverable pineapples

--- server/pineapple/service.js
+++ server/pineapple/service.js
@@ -2,8 +2,9 @@ const Pineapple = require('./model')
 const geohash = require('ngeohash')
 const EmailService = require('../shared/services/email')
 const config = require('../shared/config/index')
+
 exports.validate = ( pineapple ) => {
-  
+
   let doc = Pineapple( pineapple )
 
   return doc.validate()
@@ -11,7 +12,7 @@ exports.validate = ( pineapple ) => {
 }
 
 exports.getPineappleFromReq = ( props ) => {
-  
+
   return {
 
     streetAddress : props.data.streetAddress,
@@ -31,9 +32,9 @@ exports.getPineappleFromReq = ( props ) => {
       coordinates: [props.data.geocode.lng, props.data.geocode.lat]
 
     }
-    
+
   }
-  
+
 }
 
 exports.create = ( pineapple) => {
@@ -68,42 +69,25 @@ exports.track = trackingId => {
 
 exports.getTotalNumPineappleNotInDelivery = (pineapplesInCluster) => {
 
-  return Pineapple.count( {
+  // not in delivery = not in a cluster, not already delivered and possible to deliver
+  return Pineapple.count( { _id: { '$nin': pineapplesInCluster }, delivered: false, deliverable: true } )
 
-    _id : {
-
-      '$nin' : pineapplesInCluster
-
-    },
-    delivered : false
-
-  } )
-  
 }
 
 exports.getTotalNumPineappleInDeliveryButNotDelivered = (pineapplesInCluster) => {
 
-  return Pineapple.count( {
-
-    _id : {
-
-      '$in' : pineapplesInCluster
-
-    },
-    delivered : false
-
-  } )
+  return Pineapple.count( { _id: { '$in': pineapplesInCluster }, delivered: false } )
 
 }
 
 exports.sendTrackingEmail = pineapple => {
 
   let actionUrl = `${config.get('pineapplePageUrl')}?trackingId=${pineapple._id.toString()}`
-  return EmailService.sendToCustomerAfterOrder(pineapple.senderEmail, { 
-    
+  return EmailService.sendToCustomerAfterOrder(pineapple.senderEmail, {
+
     actionUrl : actionUrl,
     timestamp : Date.now()
-    
+
   })
 
 }
