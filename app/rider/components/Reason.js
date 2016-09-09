afda2306f3878e16e489d92b45f7225affeb3041
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2016-09-09 20:37:20 +0100

    Remove Other option and free text from frontend

--- app/rider/components/Reason.js
+++ app/rider/components/Reason.js
@@ -2,16 +2,10 @@
  * Created by BigaMasta on 8/24/16.
  */
 import React from 'react'
-import { StyleSheet, css } from 'aphrodite/no-important'
 import Select from 'react-select'
-import { button as Button, grid as Grid, input as Input } from '../../shared/components'
-import { OTHER } from '../../shared/constants/index'
+import { grid as Grid } from '../../shared/components'
 
-const Reason = ({ item, undeliveredReasonOptions, actions: { changeReason, submitChangedReason, changeReasonComment } }) => {
-
-  const renderInput = item.get('undeliveredReason') === OTHER && <Input
-      value={item.get('reasonComment') || ''}
-      onChange={e => changeReasonComment(e.target.value, item.get('originalIndex'))} />
+const Reason = ({ item, undeliveredReasonOptions, actions: { submitChangedReason } }) => {
 
   return (
     <div>
@@ -20,34 +14,12 @@ const Reason = ({ item, undeliveredReasonOptions, actions: { changeReason, submi
         <Select
           value={item.get('undeliveredReason')}
           options={undeliveredReasonOptions}
-          onChange={option => changeReason(option && option.value, item.get('originalIndex'))} />
-
-        { renderInput }
+          onChange={option => submitChangedReason(item.get('_id'), option && option.value, null, item.get('originalIndex'))} />
       </Grid>
-
-      <div className={ css(styles.centered) }>
-        <Button
-          label='Submit'
-          onClick={() => {
-
-            if (item.get('undeliveredReason') !== OTHER)
-              submitChangedReason(item.get('_id'), item.get('undeliveredReason'), null, item.get('originalIndex'))
-            else
-              submitChangedReason(item.get('_id'), item.get('undeliveredReason'), item.get('reasonComment'), item.get('originalIndex'))
-
-          }}/>
-      </div>
 
     </div>
   )
 
 }
-
-const styles = StyleSheet.create({
-  centered: {
-    margin: '20px 0',
-    textAlign: 'center',
-  },
-})
 
 export default Reason
