6196df52b69ae41a19fdd5c335319f0749b13fa1
Luke Hedger <luke.hedger@gmail.com>
2016-09-09 20:36:38 +0100

    add snackbar mmm;

--- app/rider/service.js
+++ app/rider/service.js
@@ -12,13 +12,13 @@ export const fetchClusters = (resolve, reject) =>
 export const changeStatus = (id, status, pineappleIndex, resolve, reject) =>
   API.put(`pineapple/${id}`, status)
     .then( res => res.json() )
-    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ ...json, pineappleIndex }) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ ...json, pineappleIndex, snackbar: 'Pineapple delivery status updated! 🍍' }) )
     .catch( e => reject(e) )
 
 export const changeReason = (id, reason, comment, pineappleIndex, resolve, reject) =>
   API.put(`pineapple/${id}`, { undeliveredReason: reason, reasonComment: comment })
     .then( res => res.json() )
-    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ ...json, pineappleIndex }) )
+    .then( json => json.errMsg ? reject(Error(json.errMsg)) : resolve({ ...json, pineappleIndex, snackbar: 'Pineapple undelivered reason updated! 👍' }) )
     .catch( e => reject(e) )
 
 export const startClusterDelivery = (id, resolve, reject) =>
