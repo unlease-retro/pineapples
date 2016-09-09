18c07cafff04fc8ce30df9932718e3dfd558cd54
Izzy <xinjin214@gmail.com>
2016-09-09 01:16:01 +0100

    bigamasta baby

--- app/shared/util/googleMapsLinkBuilder.js
+++ app/shared/util/googleMapsLinkBuilder.js
@@ -1,6 +1,6 @@
 /**
  * Created by BigaMasta on 9/5/16.
  */
-export const prefix = 'https://www.google.sk/maps/dir'
+export const prefix = 'https://www.google.co.uk/maps/dir'
 export const cycleRoute = '/data=!3m1!4b1!4m2!4m1!3e1'
 export const routeSteps = '/am=t'
